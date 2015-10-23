af59bb45bf056102ed83c2459063a65409c7883a
Turupawn <ahmed.hn.43@gmail.com>
2015-10-23 13:06:16 -0600

    fin de semana 1

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,11 +3,14 @@
 Jugador::Jugador(SDL_Renderer* renderer)
 {
     this->renderer = renderer;
-    this->texture = IMG_LoadTexture(renderer, "personaje.png");
+    this->textures.push_back(IMG_LoadTexture(renderer, "Personaje/down1.png"));
+    this->textures.push_back(IMG_LoadTexture(renderer, "Personaje/down2.png"));
 
-    SDL_QueryTexture(this->texture, NULL, NULL, &rect.w, &rect.h);
+    SDL_QueryTexture(this->textures[0], NULL, NULL, &rect.w, &rect.h);
     rect.x = 0;
     rect.y = 0;
+
+    current_texture=0;
 }
 
 Jugador::~Jugador()
@@ -17,7 +20,7 @@ Jugador::~Jugador()
 
 void Jugador::dibujar()
 {
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_RenderCopy(renderer, textures[current_texture], NULL, &rect);
 }
 
 void Jugador::logica()
@@ -40,4 +43,13 @@ void Jugador::logica()
     {
         rect.y+=1;
     }
+
+    if(frames%100==0)
+    {
+        current_texture++;
+        if(current_texture>=textures.size())
+            current_texture=0;
+    }
+
+    frames++;
 }
--- Jugador.h
+++ Jugador.h
@@ -3,13 +3,18 @@
 
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
+#include<vector>
+
+using namespace std;
 
 class Jugador
 {
     public:
         SDL_Renderer* renderer;
-        SDL_Texture *texture;
+        vector<SDL_Texture*>textures;
         SDL_Rect rect;
+        int current_texture;
+        int frames;
         Jugador(SDL_Renderer* renderer);
         virtual ~Jugador();
         void dibujar();
